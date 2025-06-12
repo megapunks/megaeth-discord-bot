7b102329963581f215f5714df4bb86cb3e6bd683
MegaPunks <smegapunks@gmail.com>
2025-06-13 00:53:22 +0330

    fix: valid slash command

--- src/ping.js
+++ src/ping.js
@@ -4,7 +4,6 @@ module.exports = {
   data: new SlashCommandBuilder()
     .setName('ping')
     .setDescription('Replies with Pong!'),
-    
   async execute(interaction) {
     await interaction.reply('🏓 Pong!');
   },
