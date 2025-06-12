0e6b5c7c72e6d81175417ff3ae3a86768fac0feb
MegaPunks <smegapunks@gmail.com>
2025-06-12 19:10:57 +0330

    refactor: switch to JS, secure env, finalize Railway deploy

--- src/commands/ping.js
+++ src/commands/ping.js
@@ -1,9 +1,10 @@
+const { SlashCommandBuilder } = require('discord.js');
+
 module.exports = {
-  data: {
-    name: 'ping',
-    description: 'Replies with Pong!'
-  },
+  data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('Replies with Pong!'),
   async execute(interaction) {
-    await interaction.reply('🏓 Pong!');
+    await interaction.reply('Pong!');
   }
 };
